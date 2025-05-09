676ee6b214f1e113cbb16dbfe96fc5d051c57267
Lalitha <lalitha@LalitaLallu.local>
2025-05-09 10:18:39 +0530

    removed PE

--- main.tf
+++ main.tf
@@ -53,30 +53,30 @@ resource "azurerm_key_vault_secret" "postgressql_password" {
 
   depends_on = [azurerm_postgresql_flexible_server.postgresql_flexible_server]
 }  
-# Creates  a private endpoint with private dns
-resource "azurerm_private_endpoint" "endpoint" {
-  name                = "${var.name}-pe"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = var.private_endpoint_subnet_id
+# # Creates  a private endpoint with private dns
+# resource "azurerm_private_endpoint" "endpoint" {
+#   name                = "${var.name}-pe"
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
+#   subnet_id           = var.private_endpoint_subnet_id
 
-  private_service_connection {
-    name                           = "${var.name}-connection"
-    private_connection_resource_id = azurerm_postgresql_flexible_server.postgresql_flexible_server.id
-    is_manual_connection           = var.is_manual_connection
-    subresource_names              = var.subresource_names
+#   private_service_connection {
+#     name                           = "${var.name}-connection"
+#     private_connection_resource_id = azurerm_postgresql_flexible_server.postgresql_flexible_server.id
+#     is_manual_connection           = var.is_manual_connection
+#     subresource_names              = var.subresource_names
 
-  }
+#   }
 
-  private_dns_zone_group {
-    name                 = "${var.name}-dnszone"
-    private_dns_zone_ids = var.private_dns_zone_ids
-  }
-  lifecycle {
-    ignore_changes = [
-      tags,
-    ]
-  }
-  depends_on = [azurerm_postgresql_flexible_server.postgresql_flexible_server]
+#   private_dns_zone_group {
+#     name                 = "${var.name}-dnszone"
+#     private_dns_zone_ids = var.private_dns_zone_ids
+#   }
+#   lifecycle {
+#     ignore_changes = [
+#       tags,
+#     ]
+#   }
+#   depends_on = [azurerm_postgresql_flexible_server.postgresql_flexible_server]
 
-}
+# }
